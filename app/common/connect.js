6d09d88bdbb4b457ba25d5edb011e67aa73a2b9a
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2022-03-19 12:24:38 +0700

    bug create post

--- app/common/connect.js
+++ app/common/connect.js
@@ -1,17 +1,17 @@
 import mysql from 'mysql'
 
+    // var connection = mysql.createConnection({
+    //     host: 'sql6.freesqldatabase.com',
+    //     user: 'sql6479342',
+    //     password: 'Qm2hrkbip8',
+    //     database: 'sql6479342'
+    // })
 var connection = mysql.createConnection({
-    host: 'sql6.freesqldatabase.com',
-    user: 'sql6479342',
-    password: 'Qm2hrkbip8',
-    database: 'sql6479342'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'fakebook'
 })
-// var connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'fakebook'
-// })
 
 connection.connect(function(err) {
     if (err) console.log("Connect fail");
