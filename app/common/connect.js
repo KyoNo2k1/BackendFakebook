439773e6801a1298e763287eeb06f1a1f206470c
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2022-03-19 13:07:10 +0700

    Update connect.js

--- app/common/connect.js
+++ app/common/connect.js
@@ -1,16 +1,16 @@
 import mysql from 'mysql'
-    // var connection = mysql.createConnection({
-    //     host: 'sql6.freesqldatabase.com',
-    //     user: 'sql6479342',
-    //     password: 'Qm2hrkbip8',
-    //     database: 'sql6479342'
-    // })
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'fakebook'
-})
+    var connection = mysql.createConnection({
+        host: 'sql6.freesqldatabase.com',
+        user: 'sql6479342',
+        password: 'Qm2hrkbip8',
+        database: 'sql6479342'
+    })
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'fakebook'
+// })
 
 connection.connect(function(err) {
     if (err) console.log("Connect fail");
